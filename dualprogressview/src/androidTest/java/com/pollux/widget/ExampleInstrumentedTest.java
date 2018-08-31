5892b1bdfccc0c1b75f13a62eb775587ae10384d
pollux- <sreevijayan.ak@gmail.com>
2018-08-31 18:01:27 +0530

    Travis ci configured

--- dualprogressview/src/androidTest/java/com/pollux/widget/ExampleInstrumentedTest.java
+++ dualprogressview/src/androidTest/java/com/pollux/widget/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.pollux.dualprogress.test", appContext.getPackageName());
+        assertEquals("com.pollux.widget.test", appContext.getPackageName());
     }
 }
